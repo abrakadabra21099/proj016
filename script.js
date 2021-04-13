8d26be4fac7bd5db996f61c773c73449a2773529
4i! aka /bin/bashrrY <abrakadabra21099@gmail.com>
2021-04-13 13:52:46 +0400

    Learn 42. Done.

--- script.js
+++ script.js
@@ -1,2 +1,26 @@
 'use strict';
 
+const box = document.querySelector('.box'),
+      btn = document.querySelector('button');
+// const width = box.clientWidth,
+//     height = box.clientHeight;
+// const width = box.offsetWidth,
+//     height = box.offsetHeight;
+const width = box.scrollWidth,
+     height = box.scrollHeight;
+
+console.log(`W:${width}, H:${height}`);
+
+btn.addEventListener('click', () => {
+    // box.style.height = box.scrollHeight + 'px';
+    console.log(box.scrollTop);
+});
+
+console.log(box.getBoundingClientRect().top);
+
+const style = window.getComputedStyle(box);
+
+console.log(style.display);
+
+console.log(document.documentElement.clientWidth);
+console.log(document.documentElement.scrollTop);
